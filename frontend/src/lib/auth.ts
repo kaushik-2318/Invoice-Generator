5861fcd93ebdc75176dd645d5307533ad30a34d3
kaushik-2318 <kaushikverma321@gmail.com>
2024-10-15 01:06:54 +0530

    Some Minor Changes

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -1,23 +1,20 @@
 import { jwtDecode } from 'jwt-decode';
 
-// Define the structure of the decoded token based on what fields it contains
 interface DecodedToken {
-    exp: number; // Expiration time of the token
+    exp: number;
 }
 
 export const isAuthenticated = (): boolean => {
     const token = localStorage.getItem('token');
     if (!token) {
-        return false; // No token found
+        return false;
     }
 
     try {
-        // Decode the token and ensure it matches the DecodedToken structure
         const decodedToken: DecodedToken = jwtDecode<DecodedToken>(token);
 
         const currentTime = Date.now() / 1000;
 
-        // Check if the token is expired
         if (decodedToken.exp < currentTime) {
             localStorage.removeItem('token');
             return false;
