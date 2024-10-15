c2b65f91a7b5d267a6aa776eb25e10c99021602a
kaushik-2318 <kaushikverma321@gmail.com>
2024-10-15 09:17:15 +0530

    Bug Fix

--- backend/src/puppeteerConfig.ts
+++ backend/src/puppeteerConfig.ts
@@ -3,7 +3,7 @@ import puppeteer, { Browser } from 'puppeteer';
 export const launchPuppeteer = async (): Promise<Browser> => {
     try {
         const browser = await puppeteer.launch({
-            executablePath: process.env.CHROME_EXECUTABLE_PATH || '/usr/bin/google-chrome-stable',  // Default to a path if env is not set
+            executablePath: '/usr/bin/chromium-browser',  // Default to a path if env is not set
             args: ['--no-sandbox', '--disable-setuid-sandbox'],  // For cloud environments
         });
         return browser;
